63ecc525a6fec8d545a4f18c43c0ecddf1c26937
Karissa Sticks <ksticks@ox-c4.21-school.ru>
2019-12-25 19:49:40 +0300

    validate loader

--- vm/vm_main.c
+++ vm/vm_main.c
@@ -13,8 +13,11 @@ int main(int ac, char **av)
 	while(i < ac)
 	{
 		t_vm_add_champ(&vm, (const char *)av[i]);
+		ft_printf("champ: %s\n", vm.champs[i - 1].name);
+		ft_printf("champ comment: %s\n", vm.champs[i - 1].comment);
 		++i;
 	}
+	ft_print_memory(vm.mem, 64);
 	t_vm_up(&vm);
 	while (!vm.shutdown)
 		t_vm_step(&vm);
