b8bad1b848272de2196ae1d4a4fca346dc69bd4c
Karissa Sticks <ksticks@ox-c4.21-school.ru>
2019-12-25 20:29:09 +0300

    op exec WIP

---
+++ asm/1.s
@@ -0,0 +1,5 @@
+.name           "hello_asm"
+.comment        "hello asm"
+
+ld %81,r2
+aff r2
--- vm/operation.c
+++ vm/operation.c
@@ -13,7 +13,7 @@
 #include "libft_compat.h"
 #include "vm.h"
 
-extern t_op    op_tab[17];
+extern t_op op_tab[17];
 
 t_op *read_op(const byte *ptr)
 {
@@ -28,5 +28,34 @@ t_op *read_op(const byte *ptr)
 
 void t_op_exec(t_op *op, t_proc *proc, t_vm *vm)
 {
+	int i;
+	byte args_types_byte;
+	byte *args_ptr;
+	byte arg_types[3];
+	uint args[3];
 
+	args_ptr = vm->mem + proc->pc + 2;
+	args_types_byte = *(args_ptr - 1);
+	for (i = 0; i < op->args_num; ++i)
+	{
+		arg_types[i] = (args_types_byte >> (2 * i)) & (byte)0x3;
+		if (arg_types[i] == DIR_CODE)
+		{
+			ft_memcpy(&args[i], args_ptr, DIR_SIZE); //reverse because if endian
+			args_ptr += DIR_SIZE;
+		}
+		else if (arg_types[i] == IND_CODE)
+		{
+			ft_memcpy(&args[i], args_ptr, IND_SIZE);
+			ft_memcpy(&args[i], vm->mem + proc->pc + args[i], DIR_SIZE);
+			args_ptr += IND_SIZE; //size of IND value?
+		}
+		else if (arg_types[i] == REG_CODE)
+		{
+			ft_memcpy(&args[i], args_ptr, REG_SIZE);
+			args_ptr += REG_SIZE;
+		}
+		else
+			;
+	}
 }
