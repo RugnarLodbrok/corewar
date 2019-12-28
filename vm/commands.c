561ba5c3cea7941d0d5804c239ab2891c80965ec
Redwyn Poetess <rpoetess@oa-m4.21-school.ru>
2019-12-28 16:12:16 +0300

    operation

--- vm/commands.c
+++ vm/commands.c
@@ -117,7 +117,7 @@ int 	op_ldi(t_vm *vm, t_proc *proc, void *arg1, void *arg2, void *arg3)
 
 	n1 = read_uint(vm->host_endian, arg1, 4);
 	n2 = read_uint(vm->host_endian, arg2, 4);
-	ft_memcpy(arg3, proc->pc + n1 + n2, 4);
+	write_uint(vm->host_endian, n1 + n2, arg3, 4);
 	return (1);
 }
 
@@ -128,33 +128,52 @@ int 	op_sti(t_vm *vm, t_proc *proc, void *arg1, void *arg2, void *arg3)
 
 	n2 = read_uint(vm->host_endian, arg2, 4);
 	n3 = read_uint(vm->host_endian, arg3, 4);
-	ft_memcpy(proc->pc + n3 + n2, arg1, 4);
+	write_uint(vm->host_endian, n3 + n2, arg1, 4);
 	return (1);
 }
 
 int		op_fork(t_vm *vm, t_proc *proc, void *arg1, void *arg2, void *arg3)
 {
-	t_proc	new;
+	t_proc	*new;
 	int		i;
 	int 	j;
 
+	new = (t_proc*)malloc(sizeof(t_proc));
 	i = -1;
 	j = -1;
-	new.champ_id = proc->champ_id;
-	new.pc = proc->pc;
+	new->champ_id = proc->champ_id;
+	new->pc = proc->pc;
 	//new.reg = proc->reg;
-	new.carry = proc->carry;
-	new.live = proc->carry;
-	new.delay = proc->delay;
+	new->carry = proc->carry;
+	new->live = proc->carry;
+	new->delay = proc->delay;
 	while (++i < REG_NUMBER)
 		while (++j < 4)
-			new.reg[i][j] = proc->reg[i][j];
+			new->reg[i][j] = proc->reg[i][j];
+	ft_memcpy(new,  arg1, 40);
 	return (1);
 }
 
 int 	op_lld(t_vm *vm, t_proc *proc, void *arg1, void *arg2, void *arg3)
 {
+	ft_memcpy(arg2, arg1, DIR_SIZE * sizeof(char));
+	return (1);
+}
 
+int		op_lldi(t_vm *vm, t_proc *proc, void *arg1, void *arg2, void *arg3)
+{
+	int	n1;
+	int	n2;
+
+	n1 = read_uint(vm->host_endian, arg1, 4);
+	n2 = read_uint(vm->host_endian, arg2, 4);
+	write_uint(vm->host_endian, n1 + n2, arg3, 4);
+	return (1);
+}
+
+int		op_lfork(t_vm *vm, t_proc *proc, void *arg1, void *arg2, void *arg3)
+{
+	op_fork(vm, proc, arg1, arg2, arg3);
 }
 
 int		op_aff(t_vm *vm, t_proc *proc, void *arg1, void *arg2, void *arg3)
