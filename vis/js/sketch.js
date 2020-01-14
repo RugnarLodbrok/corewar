3e4df7207518a26b34eaf4bd5be707cc66b2f795
Rugnar Lodbrok <rugnarlodbrok@gmail.com>
2020-01-14 14:27:47 +0300

    working on visualization

--- vis/js/sketch.js
+++ vis/js/sketch.js
@@ -1,23 +1,6 @@
-function button(text, callback) {
-    let button = document.createElement('input');
-    button.type = 'button';
-    button.value = text;
-    button.onclick = callback;
-    return button;
-}
-
 BYTE_W = 16;
 BYTE_H = 10;
 ASPECT = 1.3;
-
-function chunks(s, size) {
-    const numChunks = Math.ceil(s.length / size);
-    const chunks = new Array(numChunks);
-
-    for (let i = 0, o = 0; i < numChunks; ++i, o += size)
-        chunks[i] = s.substr(o, size);
-    return chunks;
-}
 
 class Proc {
     constructor(id, pc) {
@@ -37,15 +20,32 @@ class Proc {
     }
 }
 
+class Byte {
+    constructor(v) {
+        this.v = v;
+        this.bg = "#888888";
+    }
+
+    draw(sketch, x, y) {
+        sketch.push();
+        sketch.strokeWeight(0);
+        sketch.fill(this.bg);
+        sketch.rect(x, y, BYTE_W, BYTE_H);
+        sketch.fill(220);
+        sketch.text(this.v, x, y, BYTE_W, BYTE_H);
+        sketch.pop();
+    }
+}
+
 class VM {
-    constructor() {
+    constructor(sketch) {
+        this.sketch = sketch;
         this.mem = null;
         this.rows = 0;
         this.cols = 0;
         this.w = 0;
         this.h = 0;
         this.procs = [];
-        this.bg = "#888888";
     }
 
     choose_row_cols(len) {
@@ -61,36 +61,32 @@ class VM {
         return [len / best, best]
     }
 
-    mem_init(sketch, data) {
-        this.mem = data;
+    mem_init(data) {
+        this.mem = [];
+        for (let datum of data)
+            this.mem.push(new Byte(datum));
         [this.rows, this.cols] = this.choose_row_cols(this.mem.length);
         this.w = this.cols * BYTE_W;
         this.h = this.rows * BYTE_H;
         console.log(this.rows, this.cols);
-        // sketch.push();
-        // sketch.strokeWeight(0);
-        // sketch.fill("#888888");
-        // sketch.rect(0, 0, this.w, this.h);
-        // sketch.fill(220);
         for (let i = 0; i < this.mem.length; ++i)
-            this.draw_byte(sketch, i);
-        // sketch.pop();
+            this.draw_byte(i);
         console.log("done");
     }
 
-    proc_move(sketch, id, pc) {
+    proc_move(id, pc) {
         let proc = this.procs[id];
         if (!proc)
             return console.error(`proc ${id} does not exist`);
-        proc.move(sketch, this, pc);
+        proc.move(this.sketch, this, pc);
     }
 
-    new_proc(sketch, id, pc) {
+    new_proc(id, pc) {
         if (id !== this.procs.length)
-            console.error("bad new proc id");
+            console.error(`bad new proc id: ${id}, procs.len: ${this.procs.length}`);
         else
             this.procs[id] = new Proc(id, pc);
-        this.proc_move(sketch, id, pc);
+        this.proc_move(id, pc);
     }
 
     byte_coors(i) {
@@ -99,78 +95,78 @@ class VM {
             (BYTE_H * Math.floor(i / this.cols))];
     }
 
-    draw_byte(sketch, i) {
+    draw_byte(i) {
         let x;
         let y;
+        let sketch = this.sketch;
 
         [x, y] = this.byte_coors(i);
-        sketch.push();
-        sketch.strokeWeight(0);
-        sketch.fill(this.bg);
-        sketch.rect(x, y, BYTE_W, BYTE_H);
-        sketch.fill(220);
-        sketch.text(this.mem[i], x, y, BYTE_W, BYTE_H);
-        sketch.pop();
+        this.mem[i].draw(this.sketch, x, y)
     }
 
-    write_mem(sketch, pc, data) {
-        console.log("write mem", pc, data);
+    write_mem(pc, data) {
         for (let i = 0; i < data.length; ++i) {
-            this.mem[pc + i] = data[i];
-            this.draw_byte(sketch, pc + i);
+            this.mem[pc + i].v = data[i];
+            this.draw_byte(pc + i);
         }
     }
 
-    update(sketch, msg) {
+    update(msg) {
         if (msg.type === "mem_init")
-            this.mem_init(sketch, chunks(msg.data, 2));
+            this.mem_init(chunks(msg.data, 2));
         else if (msg.type === "new_proc")
-            this.new_proc(sketch, msg.id - 0, msg.pc);
+            this.new_proc(msg.id - 0, msg.pc);
         else if (msg.type === "arr")
             console.log('STDOUT:', msg.char);
         else if (msg.type === "write_mem")
-            this.write_mem(sketch, msg.pc - 0, chunks(msg.data, 2));
+            this.write_mem(msg.pc - 0, chunks(msg.data, 2));
         else if (msg.type === "proc_move")
-            this.proc_move(sketch, msg.id, msg.pc)
+            this.proc_move(msg.id, msg.pc)
     }
 }
 
+let vm_div = div();
+
 new p5(function (sketch) {
     let socket;
-    let main_div;
     let vm;
+    let main_div = document.getElementById('sketch');
+    let buttons_div = div();
+
+    main_div.appendChild(buttons_div);
+    main_div.appendChild(vm_div);
 
     sketch.setup = () => {
-        vm = new VM();
-        main_div = document.getElementById('sketch');
-        main_div.appendChild(button('start vm', function (e) {
-            if (socket)
-                socket.close(1000, "re-connecting");
-            socket = new WebSocket('ws://localhost:8765/somesocket');
+        buttons_div.appendChild(div(button('start vm', function (e) {
+                vm = new VM(sketch);
+                if (socket)
+                    socket.close(1000, "re-connecting");
+                socket = new WebSocket('ws://localhost:8765/somesocket');
 
-            socket.onopen = function () {
-                socket.send('hello');
-            };
-            socket.onmessage = function (s) {
-                let data = s.data;
-                let msg = JSON.parse(data);
-                if (!data)
-                    return console.error("empty message");
-                if (!msg)
-                    return console.error(`can't parse data: "${data}"`);
-                vm.update(sketch, msg);
-            };
-        }));
-
-        main_div.appendChild(button('send hello', function (e) {
-            console.log("send");
-            socket.send("hello from browser!");
-        }));
-
-        main_div.appendChild(button('stop vm', function (e) {
-            console.log('close!');
-            socket.close(1000, "stopping");
-        }));
+                socket.onopen = function () {
+                    socket.send('hello');
+                };
+                socket.onmessage = function (s) {
+                    let data = s.data;
+                    let msg = JSON.parse(data);
+                    if (!data)
+                        return console.error("empty message");
+                    if (!msg)
+                        return console.error(`can't parse data: "${data}"`);
+                    vm.update(msg);
+                };
+            }),
+            button('send hello', function (e) {
+                console.log("send");
+                socket.send("hello from browser!");
+            }),
+            button('stop vm', function (e) {
+                console.log('close!');
+                socket.close(1000, "stopping");
+            })
+        ));
+        vm_div = div();
+        vm_div.id = 'vm_div';
 
         sketch.createCanvas(1200, 800);
         sketch.background(0);
@@ -183,4 +179,4 @@ new p5(function (sketch) {
     };
     sketch.draw = () => {
     };
-}, document.getElementById("sketch"));
+}, vm_div);
